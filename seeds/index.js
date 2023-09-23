9cb4ae265895c19ebd206b3e01553b163efd3c5d
Sahil Batgeri <sahilbatgeri@gmail.com>
2023-09-23 22:24:51 +0530

    Added campgrounds location and title in the database

--- seeds/index.js
+++ seeds/index.js
@@ -1,6 +1,8 @@
 const mongoose = require('mongoose');
 const Campground = require('../models/campgrounds');
-
+const cities = require('./cities');
+const colors = require('colors');
+const {places , descriptors} = require('./seedHelpers');
 
 mongoose.connect('mongodb://127.0.0.1:27017/Campground').then(() => {
     console.log('database connected'.zebra);
@@ -9,8 +11,25 @@ mongoose.connect('mongodb://127.0.0.1:27017/Campground').then(() => {
     console.log('error in database connection'.zebra , err);
 })
 
+const name = (array) => array[Math.floor(Math.random()*array.length)];
+
 const seedDB = async () => {
     await Campground.deleteMany({});
+
+    for(let i = 0 ; i < 50 ;i++){
+        const randomNumber = Math.floor(Math.random()*1000);
+
+        const camp = new Campground({
+            location : `${cities[randomNumber].city}, ${cities[randomNumber].state}`,
+            title : `${name(descriptors)} ${name(places)}`
+        });
+
+        await camp.save();
+        
+    }
+
 }
 
-seedDB();
+seedDB().then(() => {
+    mongoose.connection.close();
+})
