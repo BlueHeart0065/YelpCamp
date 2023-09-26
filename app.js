e1926e78fc392b6c0adfcc06ba844068490bb313
Sahil Batgeri <sahilbatgeri@gmail.com>
2023-09-26 21:16:17 +0530

    CRUD completed added bootstrap for styling

--- app.js
+++ app.js
@@ -4,15 +4,19 @@ const mongoose = require('mongoose');
 const path = require('path');
 const colors = require('colors'); 
 const Campground = require('./models/campgrounds');
+const methodOverride = require('method-override');
+const ejsMate = require('ejs-mate');
 
 const app = express();
 const port = 3000;
 
 app.set('view engine', 'ejs');
+app.engine('ejs',ejsMate);
 app.use(express.json());
 app.use(express.urlencoded({extended : true}));
 app.use(express.static(path.join(__dirname , 'public')));
 app.use(express.static(path.join(__dirname , 'views')));
+app.use(methodOverride('_method'));
 
 mongoose.connect('mongodb://127.0.0.1:27017/Campground').then(() => {
     console.log('database connected'.zebra);
@@ -47,6 +51,21 @@ app.post('/campgrounds/new' , async (req , res) => {
     res.redirect(`/campgrounds/${camp.id}`);
 });
 
+app.get('/campgrounds/:id/edit' , async (req , res) => {
+    const id = req.params.id;
+
+    const camp = await Campground.findById(id);
+
+    res.render('edit' , {camp});
+})
+
+app.put('/campgrounds/:id/edit' , async (req , res) => {
+    const id = req.params.id;
+    const {title , location} = req.body;
+    await Campground.findByIdAndUpdate(id , {'title' : title , 'location' : location});
+    res.redirect(`/campgrounds/${id}`);
+})
+
 app.get('/campgrounds/:id' , async (req , res) => {
     const id = req.params.id;
     const campground = await Campground.findById(id);
@@ -54,6 +73,12 @@ app.get('/campgrounds/:id' , async (req , res) => {
     res.render('show' , {campground});
 });
 
+app.delete('/campgrounds/:id' , async (req , res) => {
+    const id = req.params.id;
+    
+    await Campground.findByIdAndDelete(id);
+    res.redirect('/campgrounds');
+})
 
 
 
